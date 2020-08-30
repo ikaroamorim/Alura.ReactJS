b2bc02b00a2d7cecee8d0ee29826b95d2cc8d553
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2020-08-30 19:31:13 -0300

    JsOO - Aula 02

--- 01JsOrientadoObjeto/index.js
+++ 01JsOrientadoObjeto/index.js
@@ -1,19 +1,28 @@
-class Cliente{
+class Cliente {
     id;
     nome;
     cpf;
     conta;
 }
 
-class ContaCorrente{
+class ContaCorrente {
     id;
     agencia;
-    saldo;
+    //campos privados não foram estabelecidos na linguagem ainda
+    //#saldo = 0;
+    _saldo = 0
 
-    sacar(valor){
-        if(this.saldo>=valor){
-            this.saldo -= valor;
-        }
+    sacar(valor) {
+        if(valor<=0 || this._saldo <= valor) return false;
+        this._saldo -= valor;
+        return true;
+    }
+
+    depositar(valor) {
+        if (valor <= 0) return false;
+        this._saldo += valor;
+        console.log(this._saldo);
+        return true;
     }
 }
 
@@ -22,13 +31,13 @@ const cliente1 = new Cliente();
 const conta1 = new ContaCorrente();
 
 cliente1.id = 1;
-cliente1.nome= "Ikaro";
+cliente1.nome = "Ikaro";
 cliente1.cpf = 1231231230;
 cliente1.conta = 121230;
 
 conta1.id = 121230;
 conta1.agencia = 1234;
-conta1.saldo = 5439.90;
+conta1.depositar(5439.90);
 
 console.log(cliente1);
 console.log(conta1);
