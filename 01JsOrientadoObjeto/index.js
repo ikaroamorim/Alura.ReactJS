421013f3b2b1cfef737251b50c5754a85364484a
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2020-08-30 18:56:41 -0300

    JsOO - Aula 01

---
+++ 01JsOrientadoObjeto/index.js
@@ -0,0 +1,18 @@
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    conta;
+    saldo;
+}
+
+
+const cliente1 = new Cliente();
+
+cliente1.nome= "Ikaro";
+cliente1.cpf = 1231231230;
+cliente1.agencia = 1234;
+cliente1.conta = 121230
+cliente1.saldo = 0;
+
+console.log(cliente1)
