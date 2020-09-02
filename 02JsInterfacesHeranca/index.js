53b82b80db879f6ee5fdf43d53e37cb0a49bb10f
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2020-09-01 21:16:32 -0300

    JsOO2 - Aula 01

--- 02JsInterfacesHeranca/index.js
+++ 02JsInterfacesHeranca/index.js
@@ -1,17 +1,17 @@
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import { Cliente } from "./Cliente.js"
+import { ContaCorrente } from "./ContaCorrente.js"
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./Conta.js"
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 const cliente2 = new Cliente("Alice", 88822233309);
 
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
+const conta1 = new Conta(0, cliente1, 1001);
+const conta2 = new Conta(500, cliente2, 1002);
 
-const conta2 = new ContaCorrente(102, cliente2);
+conta1.depositar(500);
+conta1.sacar(100);
 
-let valor = 200;
-contaCorrenteRicardo.tranferir(valor, conta2);
-
-console.log(contaCorrenteRicardo);
+console.log(conta1);
+console.log(conta2);
