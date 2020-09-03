5e0af8661aa125b52606c8157b9a85c307689389
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2020-09-02 21:13:19 -0300

    JsOO - Final

--- 02JsInterfacesHeranca/SistemaAutenticacao.js
+++ 02JsInterfacesHeranca/SistemaAutenticacao.js
@@ -4,6 +4,13 @@ Ser autenticavel significa ter a propriedade senha
 
 export class SistemaAutenticacao{
     static login(autenticavel, senha){
-        return autenticavel.autenticar(senha);
+        if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+    static ehAutenticavel(autenticavel){
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function;
     }
 }
